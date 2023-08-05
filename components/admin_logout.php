43247c1b22217f1b0871132cbc4d2708441df365
Khaled Zeid <i5aledzeid@gmail.com>
2023-08-05 19:50:38 +0300

    Add files via upload

--- components/admin_logout.php
+++ components/admin_logout.php
@@ -1,7 +1,6 @@
 <?php
 
 include 'connect.php';
-
 session_start();
 session_unset();
 session_destroy();
--- components/connect.php
+++ components/connect.php
@@ -3,7 +3,6 @@
 $db_name = 'mysql:host=localhost;dbname=shop_dbsz';
 $user_name = 'root';
 $user_password = '';
-
 $conn = new PDO($db_name, $user_name, $user_password);
 
 ?>
