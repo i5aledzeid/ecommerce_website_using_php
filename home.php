1b5a2a0656d9ad56ac7e084cd26cebd831ce09f2
Khaled Zeid <i5aledzeid@gmail.com>
2023-08-18 02:06:59 +0300

    Add files via upload

--- home.php
+++ home.php
@@ -334,6 +334,96 @@ include 'components/wishlist_cart.php';
 
 <section class="home-products">
 
+   <h1 class="heading">samsung products</h1>
+
+   <div class="swiper products-slider">
+
+   <div class="swiper-wrapper">
+
+       <?php
+         $select_products = $conn->prepare("SELECT * FROM `products` WHERE brand='samsung' LIMIT 6"); 
+         $select_products->execute();
+         if($select_products->rowCount() > 0){
+          while($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)){
+       ?>
+       <form action="" method="post" class="swiper-slide slide">
+          <input type="hidden" name="pid" value="<?= $fetch_product['id']; ?>">
+          <input type="hidden" name="name" value="<?= $fetch_product['name']; ?>">
+          <input type="hidden" name="price" value="<?= $fetch_product['price']; ?>">
+          <input type="hidden" name="image" value="<?= $fetch_product['image_01']; ?>">
+          <input type="hidden" name="store" value="<?= $fetch_product['created_by']; ?>">
+          <input type="hidden" name="sid" value="<?= $fetch_product['sid']; ?>">
+          <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
+          <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
+          <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
+          <div class="name" style="font-weight: bold;"><?= $fetch_product['name']; ?></div>
+            <div class="name" style="font-size: 12px;">
+                <i class="bi bi-tags-fill"></i> <a href="category.php?category=<?php echo $fetch_product['category']; ?>">
+                    <?= $fetch_product['category']; ?>
+                    </a> (<?= $fetch_product['brand']; ?>)
+            </div>
+          <div class="name" style="font-size: 16px; color: #198754;">
+            <i class="bi bi-shop"></i> <?= '[' . $fetch_product['sid'] . '] ' . $fetch_product['created_by']; ?>
+            <?php
+            $by = $fetch_product['created_by'];
+            $select_stores = $conn->prepare("SELECT * FROM `store` WHERE `title`='$by'"); 
+            $select_stores->execute();
+            if($select_stores->rowCount() > 0){
+                while($fetch_store = $select_stores->fetch(PDO::FETCH_ASSOC)){ ?>
+                    <?php
+                        $status = $fetch_store['status'];
+                        if ($status == 0) {
+                            echo '<i class="fa fa-info-circle" style="color: #0D6EFD;" aria-hidden="true" rel="tooltip" title="جديد" id="blah"></i>';
+                        } else if ($status == 1) {
+                            echo '<i class="bi bi-exclamation-triangle" style="color: #F58F3C;" rel="tooltip" title="حظر مؤقت" id="blah"></i>';
+                        } else if ($status == 2) {
+                            echo '<i class="bi bi-exclamation-circle" style="color: #6C757D;" rel="tooltip" title="بإنتظار التوثيق" id="blah"></i>';
+                        } else if ($status == 3) {
+                            echo '<i class="fa fa-check" style="color: #198754;" aria-hidden="true" rel="tooltip" title="تم التوثيق" id="blah"></i>';
+                        } else if ($status == 4) {
+                            echo '<i class="bi bi-sign-stop-fill" style="color: #DC3545;" rel="tooltip" title="حظر تام" id="blah"></i>';
+                        } else if ($status == 5) {
+                            echo '<i class="bi bi-coin" style="color: #198754;" rel="tooltip" title="سوق محترف" id="blah"></i>';
+                        } else if ($status == 6) {
+                            echo '<i class="bi bi-patch-check-fill" style="color: #1D9BF0; font-size: 18px;" rel="tooltip" title="المالك" id="blah"></i>';
+                        }
+                    
+                }
+            }
+            ?>
+            <script>
+                $(document).ready(function() {
+                    $("[rel=tooltip]").tooltip({ placement: 'right'});
+                });
+            </script>
+        </div>
+          <div class="flex">
+             <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
+             <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
+          </div>
+        <?php if ($user_id != $fetch_product['sid']) { ?>
+            <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+        <?php } else { ?>
+            <input type="button" value="لا يمكن إضافة منتج من نفس السوق" class="btn" name="" style="background: white; color: black;">
+        <?php } ?>
+       </form>
+       <?php
+          }
+       }else{
+          echo '<p class="empty">no products added yet!</p>';
+       }
+       ?>
+
+   </div>
+
+   <!--<div class="swiper-pagination"></div>-->
+
+   </div>
+
+</section>
+
+<section class="home-products">
+
     <h1 class="heading">
        list stores
        <?php
--- shop.php
+++ shop.php
@@ -12,6 +12,37 @@ if(isset($_SESSION['user_id'])){
 
 include 'components/wishlist_cart.php';
 
+/*************************** SORT ***************************/
+if (isset($_POST['submit'])) {
+    $lang = $_POST['myvalue'];
+    //echo "<script>alert('$lang');</script>";
+}
+else if ($_POST['submit'] == '') {
+    //echo "<script>alert('$lang');</script>";
+}
+if (isset($_POST['submit'])) {
+    $brand = $_POST['mybrand'];
+    //echo "<script>alert('$brand');</script>";
+}
+else if ($_POST['submit'] == '') {
+    //echo "<script>alert('$brand');</script>";
+}
+if (isset($_POST['submit'])) {
+    $price = $_POST['myprice'];
+    //echo "<script>alert('$price');</script>";
+}
+else if ($_POST['submit'] == '') {
+    //echo "<script>alert('$price');</script>";
+}
+if (isset($_POST['submit'])) {
+    $time = $_POST['mytime'];
+    //echo "<script>alert('$time');</script>";
+}
+else if ($_POST['submit'] == '') {
+    //echo "<script>alert('$time');</script>";
+}
+/*************************** SORT ***************************/
+
 ?>
 
 <!DOCTYPE html>
@@ -38,12 +69,148 @@ include 'components/wishlist_cart.php';
 
 <section class="products">
 
-   <h1 class="heading">latest products</h1>
-
+   <h1 class="heading">latest products <a style="color: blue;">Sort</a>
+        <!--*************************** SORT ***************************-->
+        <style>
+            select, option {
+                font-size: 12px;
+            }
+            .form-select {
+                padding: 8px;
+                margin-bottom: -16px;
+                border-radius: 16px;
+            }
+            @media only screen and (max-width: 600px) {
+                select, option {
+                    font-size: 12px;
+                }
+                select {
+                    /* top left bottom right */
+                    padding: 4px 0px 4px 0px;
+                    margin-bottom: -16px;
+                    border-radius: 16px;
+                    color: black;
+                }
+            }
+        </style>
+        <form action="" method="POST">
+            <select class="form-select" aria-label="Default select example" name="myprice" id="myprice">
+                <?php
+                    echo '<option value="By price">By price</option>';
+                    if ($price == '') {
+                        echo '<option selected>By price</option>';
+                    }
+                    else if ($price == 'By price') {
+                        echo '<option selected>By price</option>';
+                    }
+                    else {
+                        echo '<option selected>'.$price.'</option>';
+                    }
+                ?>
+                <option value="ASC">Low</option>
+                <option value="DESC">High</option>
+            </select>
+            <select class="form-select" aria-label="Default select example" name="mytime" id="mytime">
+                <?php
+                    echo '<option value="By time">By time</option>';
+                    if ($time == '') {
+                        echo '<option selected>By time</option>';
+                    }
+                    else if ($time == 'By time') {
+                        echo '<option selected>By time</option>';
+                    }
+                    else {
+                        echo '<option selected>'.$time.'</option>';
+                    }
+                ?>
+                <option value="ASC">Old</option>
+                <option value="DESC">New</option>
+            </select>
+            <select class="form-select" aria-label="Default select example" name="mybrand" id="mybrand">
+                <?php
+                    echo '<option value="By brand">By brand</option>';
+                    if ($brand == '') {
+                        echo '<option selected>By brand</option>';
+                    }
+                    else if ($brand == 'By brand') {
+                        echo '<option selected>By brand</option>';
+                    }
+                    else {
+                        echo '<option selected>'.$brand.'</option>';
+                    }
+                ?>
+                <option value="apple">Apple</option>
+                <option value="samsung">Samsung</option>
+                <option value="lg">LG</option>
+            </select>
+            <select class="form-select" aria-label="Default select example" name="myvalue" id="myvalue">
+                <?php
+                    echo '<option value="By category">By category</option>';
+                    if ($lang == '') {
+                        echo '<option selected>By category</option>';
+                    }
+                    else if ($lang == 'By category') {
+                        echo '<option selected>By category</option>';
+                    }
+                    else {
+                        echo '<option selected>'.$lang.'</option>';
+                    }
+                ?>
+                <option value="laptop">Laptop</option>
+                <option value="television">Television</option>
+                <option value="camera">Camera</option>
+                <option value="mouse">Mouse</option>
+                <option value="fridge">Fridge</option>
+                <option value="washing">Washing Machine</option>
+                <option value="smartphone">Smartphone</option>
+                <option value="watch">Watch</option>
+            </select>
+            <button type="submit" name="submit" style="padding: 8px 16px 8px 16px; border-radius: 16px; background: #FE4445; color: white; cursor: pointer;">بحث متقدم</button>
+        </form>
+        <!--*************************** SORT ***************************-->
+   </h1>
+    
    <div class="box-container">
 
    <?php
-     $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id'"); 
+     //$select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id'"); 
+     /*if ($lang == 'all' || $lang == '' || $lang == 'By category') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id'"); 
+     }
+     if ($brand == 'all' || $brand == '' || $brand == 'By brand') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id'"); 
+     }
+     if ($price == 'all' || $price == '' || $price == 'By price') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id'"); 
+     }*/
+     /*else if ($price == 'DESC') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' AND category='$lang' OR brand='$brand' ORDER BY `price` $price"); 
+     }
+     else if ($price == 'ASC') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' AND category='$lang' OR brand='$brand' ORDER BY `price` $price"); 
+     }*/
+     if ($lang == $lang && $lang != 'By category' && $lang != '') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' AND category='$lang'"); 
+     }
+     else if ($brand == $brand && $brand != 'By brand' && $brand != '') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' AND brand='$brand'"); 
+     }
+     else if ($time == 'DESC') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' ORDER BY `id` $time"); 
+     }
+     else if ($time == 'ASC') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' ORDER BY `id` $time"); 
+     }
+     else if ($price == 'DESC') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' ORDER BY `price` $price"); 
+     }
+     else if ($price == 'ASC') {
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' ORDER BY `price` $price"); 
+     }
+     else {
+        //$select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id' AND category='$lang' AND brand='$brand'"); 
+        $select_products = $conn->prepare("SELECT * FROM `products` WHERE sid!='$user_id'"); 
+     }
      $select_products->execute();
      if($select_products->rowCount() > 0){
       while($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)){
